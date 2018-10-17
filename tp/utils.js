8f73c1fb2eb0201dfec9cfbe06c335ec6d888d93
Lenophie <quasiphies@gmail.com>
2018-10-17 16:53:36 +0200

    Fix more utils bugs

--- tp/utils.js
+++ tp/utils.js
@@ -1,32 +1,41 @@
 const show = (refOrId) => {
-	if (typeof refOrId === "string") elem = document.getElementById(refOrId);
-    elem.style.display = "block";
+	if (typeof refOrId === "string") {
+        const elem = document.getElementById(refOrId);
+        if (elem != null) elem.style.display = "block";
+    }
 };
 
 const hide = (refOrId) => {
-	if (typeof refOrId === "string") elem = document.getElementById(refOrId);
-    elem.style.display = "none";
+	if (typeof refOrId === "string") {
+	    const elem = document.getElementById(refOrId);
+        if (elem != null) elem.style.display = "none";
+    }
+
 };
 
 const html = (refOrId, contenu) => {
 	if (typeof refOrId === "string") {
-        elem = document.getElementById(refOrId);
-        if (contenu != null) elem.innerHTML = contenu;
-        else return elem.innerHTML;
+        const elem = document.getElementById(refOrId);
+        if (elem != null) {
+            if (contenu != null) elem.innerHTML = contenu;
+            else return elem.innerHTML;
+        }
     }
 };
 
 const val = (refOrId, contenu) => {
 	if ((typeof refOrId) === "string") {
-        elem = document.getElementById(refOrId);
-        if (elem.type === "checkbox") {
-            if (contenu != null && (contenu === true || contenu === false)) elem.checked = contenu;
-            else return elem.checked;
-            return;
-        }
+        const elem = document.getElementById(refOrId);
+        if (elem != null) {
+            if (elem.type === "checkbox") {
+                if (contenu != null && (contenu === true || contenu === false)) elem.checked = contenu;
+                else return elem.checked;
+                return;
+            }
 
-        if (contenu != null) elem.value = contenu;
-        else return elem.value;
+            if (contenu != null) elem.value = contenu;
+            else return elem.value;
+        }
     }
 };
 

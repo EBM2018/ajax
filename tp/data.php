c23500240e8f2e61b47fcc3c9aa1a8b5f00429a9
tb59 <thomas.bourdeaudhuy@gmail.com>
2018-10-17 16:46:08 +0200

    init branche suggest

--- tp/data.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "La page data a été appelée par la méthode " . $_SERVER["REQUEST_METHOD"] . "\n"; 
-if (isset($_REQUEST["saisie"]))
-	echo "Texte reçu : " . $_REQUEST["saisie"]; 
-else 
-	echo "Aucun texte reçu"; 
-?>

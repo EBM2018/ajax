2008d2d3aedc978acb70a73060527889ed9959a7
Lenophie <quasiphies@gmail.com>
2018-10-17 19:34:25 +0200

    Add fuzzy search with complete list of EBM members

--- EBMSearch/search/index.php
+++ EBMSearch/search/index.php
@@ -1,3 +1,35 @@
 <?php
+require_once '../vendor/autoload.php';
 
-echo json_encode(['Thomas', 'Nans', 'Nathan', 'Valentin']);
+$EBMMembersNames = [
+    'Thomas Bourdheauhuy',
+    'Cyril Brajon',
+    'Baptiste Debever',
+    'Felix Delcourt',
+    'Nans Dumortier',
+    'Florian Heymes',
+    'Baptiste Lalanne',
+    'Baudouin Laloux',
+    'Simon Leder',
+    'Maxime Legal',
+    'Quentin Leuliet',
+    'Nathan Malnoury',
+    'Hugo Mottez',
+    'Hamza Ouhssaine',
+    'Gabin Vallet',
+    'Axel Vanet-Mas',
+    'Arthur Wirbel'
+];
+
+if (isset($_GET['q'])) {
+    // sleep(10); // Uncomment this line to test the loading icon
+    $fuse = new \Fuse\Fuse($EBMMembersNames, [
+        "threshold" => 0.4
+    ]);
+
+    $fuseResultsIds = $fuse->search($_GET['q']);
+    $fuseResults = [];
+    foreach ($fuseResultsIds as $fuseResultsId) array_push($fuseResults, $EBMMembersNames[$fuseResultsId]);
+
+    echo json_encode($fuseResults);
+} else http_response_code(422);

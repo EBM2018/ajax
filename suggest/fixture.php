984f5980fb24e7d41e4c80b6b5027498e3d2b8ad
tb59 <thomas.bourdeaudhuy@gmail.com>
2018-10-23 14:17:46 +0200

    couche integration avec fixture

---
+++ suggest/fixture.php
@@ -0,0 +1,11 @@
+<?php
+// Bonne pratique 1) attendre pour percevoir le site dans des conditions "réelles"
+sleep(2); // attendre 2s avant de fournir la réponse 
+
+// Bonne pratique 2) tester la couche de fourniture de données en faisant des requêtes directes AVANT d'essayer une requête asynchrone
+?>
+<li>Thibault</li>
+<li>Thimothe</li>
+<li>Thomas</li>
+
+
